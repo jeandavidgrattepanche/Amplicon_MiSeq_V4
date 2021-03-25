6d03425d11c1e73d3f26ad94e28401438d91c2d9
Jean-David Grattepanche <jeandavid.grattepanche@gmail.com>
2021-03-25 18:19:24 -0400

    Create 1_pool_rename_vHPC.py

---
+++ Miseq_scripts/1_pool_rename_vHPC.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+__author__ = "Jean-David Grattepanche"
+__version__ = "2, October 17, 2016"
+__email__ = "jeandavid.grattepanche@gmail.com"
+
+
+import string
+import re
+import sys
+import os
+from sys import argv
+from Bio import SeqIO
+seqlist = []
+OTUlist = []
+duplicatelist = []
+sampledict = {}
+
+def main():
+	script, seqfile,  Listsample = argv
+	for samplecode in open(Listsample,'r'):
+		if samplecode.startswith("RWS"):
+			sampledict[samplecode.split('\t')[0]] = samplecode.split('\t')[1].split('\n')[0]
+#			print(samplecode.split('\t')[0], samplecode.split('\t')[1].split('\n')[0])
+	for seq in SeqIO.parse(open(seqfile,'r'),'fasta'):
+		sampleRSW = seqfile.split('/')[-1].split('_')[0]
+#		print(seqfile, sampleRSW)
+		samplename = sampledict[sampleRSW]			
+		out = open(('/').join(seqfile.split('/')[:-1]) + "/readpooled.fas",'a')
+		out.write('>' + samplename + '_' + seq.id + '\n' + str(seq.seq) +'\n')
+		out.close()
+main()
