ad660a4a43e4bcb49badfef0d94215ada77cfad2
Jean-David Grattepanche <jeandavid.grattepanche@gmail.com>
2021-03-25 21:11:33 -0400

    Create 6_BLASTn_Vsearch_bacteria.py

---
+++ Miseq_scripts/6_BLASTn_Vsearch_bacteria.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+
+__author__ = "Jean-David Grattepanche"
+__version__ = "3, March 2, 2017"
+__email__ = "jeandavid.grattepanche@gmail.com"
+
+
+
+import sys, os, re, time, string, os.path
+from distutils import spawn
+from Bio import SeqIO
+from sys import argv
+
+vsearch_path = spawn.find_executable("vsearch")
+SSU_db = "db_v4/pr2_version_4.13.0_16S_UTAX.fasta" ##change to the correct database
+blastdict = {}
+
+def getBLAST( NGSfile, idmin, qcov, Taxa, readcutoff):
+	print("start BLAST SSU_Euk_pr2_version_4.13.0")
+	outputpath = NGSfile.split('chimeras')[0]
+	outblast = open(outputpath+'/VsearchBLAST_bact.tsv','w+')
+	ublast_self = vsearch_path + ' --usearch_global '+NGSfile+' --db '+SSU_db+ ' --strand both --id '+str(idmin/100)+' --query_cov '+ str(qcov/100)+' --blast6out '+outputpath+'/VsearchBLAST_bact.tsv ' ## No -evalue 1e-15 as usearch
+	print(ublast_self)
+	os.system(ublast_self)
+	for blast_record in open(outputpath+'/VsearchBLAST_bact.tsv','r'):
+		if blast_record.split('\t')[0] not in blastdict.values():
+			blastdict[blast_record.split('\t')[0]] = blast_record.split('\n')[0]
+		else:
+			print(blast_record, "duplicated")
+	outseq = open(outputpath+'taxonomic_assignment/Seq_reads_nochimera_nosingleton_vsearch_pr2_4.13.0_bact.fasta','w+')
+	for seq in SeqIO.parse(NGSfile,'fasta'):
+		try:
+			blastdict[seq.id]
+			ident =  blastdict[seq.id].split('\t')[2]
+			seqmatch = float(blastdict[seq.id].split('\t')[3]) -float(blastdict[seq.id].split('\t')[5])
+			cnt = blastdict[seq.id].split('\t')[3] 
+			Sim = round(float(seqmatch-int(blastdict[seq.id].split('\t')[4])) / float(cnt) * 100)
+			ID = blastdict[seq.id].split('\t')[1]
+			seqused = 1 + int(blastdict[seq.id].split('\t')[7]) - int(blastdict[seq.id].split('\t')[6])
+			cov = round(float(seqused) / float(len(seq.seq)) * 100)
+			outseq = open(outputpath+'taxonomic_assignment/Seq_reads_nochimera_nosingleton_vsearch_pr2_4.13.0_bact.fasta','a')
+			if int(seq.description.split('_')[1].replace('r','')) > (int(readcutoff)-1):
+				outseq.write('>'+seq.description+ '_'+ ID.split('_rid_')[0] + '_' +str(cov)+'_'+ str(Sim) + '%\n'+str(seq.seq) + '\n')
+				outseq.close()
+
+# 			else:
+# 				print(seq.id, 'blasted with', ID.split(';size=')[0] , " at ", ident , "% and coverage:", cov , " BUT not ", Taxa)
+# 				outseqnSAR = open('outputs/taxonomic_assignment/Seq_reads_nochimera_nosingleton_SAR_vsearch_notSAR.fasta','a')
+# 				outseqnSAR.write('>'+seq.description+ '_'+ ID.split('_rid_')[0] + '_' +str(cov)+'_'+ str(Sim) + '%\n'+str(seq.seq) + '\n')
+# 				outseqnSAR.close()
+				
+
+		except:
+			print("NO BLAST for ",seq.id)
+			outseq = open(outputpath+'taxonomic_assignment/Seq_reads_nochimera_nosingleton_vsearch_pr2_4.13.0_bact.fasta','a')
+			if int(seq.description.split('_')[1].replace('r','')) > (int(readcutoff)-1):
+				outseq.write('>'+seq.description+ '_No_BLASTrecord\n'+str(seq.seq) + '\n')
+				outseq.close()
+		
+def main():
+	script,  NGSfile, idminy, qcovz, Taxa, readcutoff = argv
+	getBLAST(NGSfile, float(idminy),float(qcovz), Taxa, readcutoff)
+main()
