44d99c343d0c722bfe197dfd7778d3cac2f75099
Jean-David Grattepanche <jeandavid.grattepanche@gmail.com>
2022-09-08 12:30:04 -0400

    Update 9_randomly_subsample_ingroup_notree_faster.py
    test in progress

--- Miseq_scripts/9_randomly_subsample_ingroup_notree_faster.py
+++ Miseq_scripts/9_randomly_subsample_ingroup_notree_faster.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 #if many more OTU than 100,000 this version should be faster
-
-
 __author__ = "Jean-David Grattepanche"
 __version__ = "5.2, September 8,2022"
 __email__ = "jeandavid.grattepanche@gmail.com"
@@ -14,40 +12,49 @@ from Bio import SeqIO
 from sys import argv
 from random import randrange
 
-samples = []; tokeep = []; readpersamplesdict= {}; listreaddict= {}
+u=0;samples = []; tokeep = []; readpersamplesdict= {}; listreaddict= {}
 
 def countread(seqfile, readmap,samplelist): 	
+	u = 0 
 	for seq in SeqIO.parse(open(seqfile,'r'),'fasta'):
 		tokeep.append(seq.id.split('_')[0])
-
-	outlog = open('readpersample_cleaned.txt','w+')
+		u+=1
+		print(int(u), end ='\r')
+	print('\n\n\n',len(tokeep),'\n\n\n')
+	outlog = open('readpersample_cleaned_test.txt','w+')
 	folder = ('/').join(readmap.split('/')[:-1])
-# 	print(folder)
+	print("saving folder: ", folder)
 	maxread = 0
 	for sample in open(samplelist,'r'):
 		samples.append(sample.replace('_','-').split('\t')[1].split('\n')[0])
 		listreaddict.setdefault(sample.replace('_','-').split('\t')[1].split('\n')[0], [])
-		readnumber= 0 
+	print(samples)
 	for line in open(readmap,'r'):
 		OTUID = line.split('\t')[0]
+		print('\n',OTUID)
 		if OTUID in tokeep:
-				for read in line.split('\t'  )[1:]:
-					samplename = ("-").join(read.replace(" ","").replace("'","").split('_')[:-1])
-		# 				print(samplename)
-					if samplename == sample.replace('_','-').split('\t')[1].split('\n')[0]:
-						readnumber=readnumber + 1
-						listreaddict[sample.replace('_','-').split('\t')[1].split('\n')[0]].append(OTUID+";"+read)
+			for read in line.split('\t'  )[1:]:
+				samplename = ("-").join(read.replace(" ","").replace("'","").split('_')[:-1])
+				print(samplename,end='\t')
+				if samplename in samples:
+					print("added",end='\t')
+					listreaddict[samplename].append(OTUID+";"+read)
 # 			else:
 # 				print(OTUID, ' was removed in previous step')
 
-		readpersamplesdict[sample.replace('_','-').split('\t')[1].split('\n')[0]] = str(readnumber)
-		print(sample.replace('_','-').split('\t')[1].split('\n')[0], " has ", str(readnumber), " reads.") 
+	readnumber= 0 
+	for sampler in open(samplelist,'r'):
+		readnumber= len(listreaddict[sampler.replace('_','-').split('\t')[1].split('\n')[0]])
+
+		readpersamplesdict[sampler.replace('_','-').split('\t')[1].split('\n')[0]] = str(readnumber)
+		print(sampler.replace('_','-').split('\t')[1].split('\n')[0], " has ", str(readnumber), " reads.") 
 		if readnumber > maxread:
 			maxread = readnumber
 # 			print(maxread)
-		outlog = open('readpersample_cleaned.txt','a')
-		outlog.write( sample.replace('_','-').split('\t')[1].split('\n')[0]+ "\t"+ str(readnumber)+'\n')
+		outlog = open('readpersample_cleaned_test.txt','a')
+		outlog.write( sampler.replace('_','-').split('\t')[1].split('\n')[0]+ "\t"+ str(readnumber)+'\n')
 		outlog.close()
+		readnumber= 0
 	outlog.close()	
 
 	subsamp = input('Do you need to subsample? (yes or no) \n')
@@ -71,7 +78,7 @@ def countread(seqfile, readmap,samplelist):
 		print ('Please answer yes or no. ')
 		main()
 
-	outfile = open(folder+'/subsampled.txt','w+')
+	outfile = open(folder+'/subsampled_test.txt','w+')
 	for sample2 in open(samplelist,'r'):
 		samplelistname = sample2.replace('_','-').split('\t')[1].split('\n')[0]	
 		if subsamp[0] == 'n':
@@ -91,13 +98,13 @@ def countread(seqfile, readmap,samplelist):
 						count = count + 1
 				for k in read_to_keep:
 					name = listreaddict[samplelistname][k]
-					outfile = open(folder+'/subsampled.txt','a')
+					outfile = open(folder+'/subsampled_test.txt','a')
 					outfile.write(name.replace('\n','').replace(' ','') + '\n')
 			else:
 				print(samplelistname, " should not be included because it contains few reads (", readpersamplesdict[samplelistname], ")")
 				for k in range(0, int(readpersamplesdict[samplelistname])):
 					name = listreaddict[samplelistname][k]
-					outfile = open(folder+'/subsampled.txt','a')
+					outfile = open(folder+'/subsampled_test.txt','a')
 					outfile.write(name.replace('\n','').replace(' ','') + '\n')
 		outfile.close()		
 	
