e2c6b5d8b449f0e97e07eaee0b087e1f6c334d9b
Jean-David Grattepanche <jeandavid.grattepanche@gmail.com>
2023-07-24 11:40:07 -0400

    Update ext_remove_N_in_seqfile_v2.py

--- script/ext_remove_N_in_seqfile_v2.py
+++ script/ext_remove_N_in_seqfile_v2.py
@@ -19,27 +19,33 @@ def main():
 	script, seqfile = argv
 	for seq in SeqIO.parse(open(seqfile,'r'),'fasta'):
 		newseq=str(seq.seq)
-		if "N" in str(seq.seq):
-# 			print(seq.description, str(seq.seq))
-			while newseq[-1] == 'N':
-				print('end:',seq.id,newseq[-1],len(newseq),end='\r')
-				newseq = newseq[:-1]
-			while newseq[0] == 'N':
-				print('beg:',seq.id,newseq[0],len(newseq),end='\r')
-				newseq = newseq[1:]
-			else:
-# 				print('\r')
-				if 'N' in newseq:
-					out = open(seqfile.split('.fasta')[0] + "_withNv2.fas",'a')
-					out.write('>' + seq.description + '\n' + newseq +'\n')
-					out.close()
+		if len(newseq) > newseq.count("N"):
+			if "N" in str(seq.seq):
+	# 			print(seq.description, str(seq.seq))
+				while newseq[-1] == 'N':
+					print('end:',seq.id,newseq[-1],len(newseq),end='\r')
+					newseq = newseq[:-1]
+				while newseq[0] == 'N':
+					print('beg:',seq.id,newseq[0],len(newseq),end='\r')
+					newseq = newseq[1:]
 				else:
-					out1 = open(seqfile.split('.fasta')[0] + "_noN.fas",'a')
-					out1.write('>' + seq.description + '\n' + newseq +'\n')
-					out1.close()
+	# 				print('\r')
+					if 'N' in newseq:
+						out = open(seqfile.split('.fasta')[0] + "_withNv2.fas",'a')
+						out.write('>' + seq.description + '\n' + newseq +'\n')
+						out.close()
+					else:
+						out1 = open(seqfile.split('.fasta')[0] + "_noN.fas",'a')
+						out1.write('>' + seq.description + '\n' + newseq +'\n')
+						out1.close()
+			else:
+				out1 = open(seqfile.split('.fasta')[0] + "_noN.fas",'a')
+				out1.write('>' + seq.description + '\n' + newseq +'\n')
+				out1.close()
 		else:
-			out1 = open(seqfile.split('.fasta')[0] + "_noN.fas",'a')
-			out1.write('>' + seq.description + '\n' + newseq +'\n')
-			out1.close()
-		
+			print(seq, "ONLY N")
+			out = open(seqfile.split('.fasta')[0] + "_withNv2.fas",'a')
+			out.write('>' + seq.description + '\n' + newseq +'\n')
+			out.close()
+
 main()
